b0a89f8aded3210a00d8ad8702cfa5cb45128c17
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-01-31 15:24:51 +0100

    make header responsive

--- src/components/Hero/Header/index.tsx
+++ src/components/Hero/Header/index.tsx
@@ -5,7 +5,7 @@ import email from "../assets/email.svg";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center py-6 font-semibold px-2">
+    <header className="flex flex-col lg:flex-row  justify-between items-center py-6 font-semibold px-2 gap-4">
       <nav>
         <ul className="flex items-center gap-8">
           <li className="flex items-center gap-4 text-xl relative font-bold select-none">
@@ -26,20 +26,24 @@ const Header = () => {
           </li>
         </ul>
       </nav>
-      <div className="flex gap-8 items-center h-full">
-        <a href="">Sign In</a>
-        <a
-          href=""
-          className="font-extrabold text-transparent bg-clip-text bg-left-top underline"
-          style={{ backgroundImage: `url(${textBackground})` }}
-        >
-          Book a Demo
-          <div
-            className="w-full h-[2px]"
-            style={{ background: `url(${textBackground})` }}
-          />
-        </a>
-        <div className="flex p-1 ring-1 text-white text-opacity-50 ring-[#ffffff33] rounded-lg h-full pl-4 gap-4">
+      <div className="flex lg:gap-8 gap-2 items-center flex-col lg:flex-row">
+        <div className="flex gap-8">
+          <a href="" className="shrink-0">
+            Sign In
+          </a>
+          <a
+            href=""
+            className="font-extrabold text-transparent bg-clip-text bg-left-top underline shrink-0"
+            style={{ backgroundImage: `url(${textBackground})` }}
+          >
+            Book a Demo
+            <div
+              className="w-full h-[2px]"
+              style={{ background: `url(${textBackground})` }}
+            />
+          </a>
+        </div>
+        <div className="flex p-1 ring-1 text-white text-opacity-50 ring-[#ffffff33] rounded-lg h-full pl-4 gap-4 items-center ">
           <img src={email} alt="helpjuice" draggable="false" />
           <input
             type="email"
